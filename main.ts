14e72fb0ae43b8cf98112f369704c0178153ac1e
cheimhus <cheimhus@gmail.com>
2025-11-11 15:28:59 +0100

    Map

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const Plant = SpriteKind.create()
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     MainCharacter,
@@ -54,15 +57,66 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sp
     tiles.setCurrentTilemap(tilemap`level 1`)
     MainCharacter.x = 230
 })
+let Wheat = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food)
+let Wheat_dead = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Plant)
+music.play(music.createSong(assets.song`mySong`), music.PlaybackMode.LoopingInBackground)
 let MainCharacter: Sprite = null
 tiles.setCurrentTilemap(tilemap`level 1`)
 MainCharacter = sprites.create(assets.image`Main`, SpriteKind.Player)
 info.setLife(3)
 MainCharacter.setPosition(40, 140)
+Wheat.setPosition(41, 15)
 game.onUpdate(function () {
     scene.cameraFollowSprite(MainCharacter)
     controller.moveSprite(MainCharacter, 50, 50)
 })
-forever(function () {
-    MainCharacter.setKind(SpriteKind.Player)
+game.onUpdateInterval(500, function () {
+    animation.runImageAnimation(
+    Wheat,
+    assets.animation`myAnim`,
+    200,
+    false
+    )
+})
+game.onUpdateInterval(500, function () {
+    animation.runImageAnimation(
+    Wheat_dead,
+    assets.animation`myAnim0`,
+    200,
+    false
+    )
 })
